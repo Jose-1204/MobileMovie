86428f440f769c47ca5bf1132fc07c2162e030c5
Jose <Jose0156.114@gmail.com>
2025-08-24 22:23:58 -0600

    chore: format code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500 ">
-        Hello world!
-      </Text>
+      <Text className="text-xl font-bold text-blue-500 ">Hello world!</Text>
     </View>
   );
 }
--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,14 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-const MovieDetails  = () => {
+const MovieDetails = () => {
   return (
     <View>
-      <Text>MovieDetails
-    
-       </Text>
+      <Text>MovieDetails</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MovieDetails 
+export default MovieDetails;
