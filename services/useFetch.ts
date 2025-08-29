5ed78c32f78e3edb4802622b0aecffb483257a20
Jose <Jose0156.114@gmail.com>
2025-08-28 18:21:59 -0600

    chore: format useFetch.ts

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,34 +1,33 @@
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
-    const [data, setData] = useState<T | null>(null);
-    const [loading, setLoading] = useState<boolean>(autoFetch);
-    const [error, setError] = useState<Error | null>(null);
-    
-    const fetchData = async () => {
-        try{ 
-            setLoading(true);
-            setError(null);
-            const result = await fetchFunction();
-            setData(result);
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(autoFetch);
+  const [error, setError] = useState<Error | null>(null);
 
-        } catch (err) {
-            setError(err instanceof Error ? err : new Error("AN ERROR OCCURRED"));
-        } finally {
-            setLoading(false);
-        }
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error("AN ERROR OCCURRED"));
+    } finally {
+      setLoading(false);
     }
-    const reset = () => {
-        setData(null);
-        setLoading(false);
-        setError(null);
+  };
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
     }
-    useEffect(() => {
-        if (autoFetch) {
-            fetchData();
-        }
-    },[]) ;
-    return { data, loading, error, refetch: fetchData, reset};
-};    
+  }, []);
+  return { data, loading, error, refetch: fetchData, reset };
+};
 
 export default useFetch;
