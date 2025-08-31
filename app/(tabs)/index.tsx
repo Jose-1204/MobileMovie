27c849002e541d06e2fb45eaddccb7551b207ec2
Jose <Jose0156.114@gmail.com>
2025-08-30 23:54:02 -0600

    add flatlist and removing unnecessary ScrollView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,7 @@
 import SearchBar from "@/components/MainSearchBar"; // search bar component
 import { icons } from "@/constants/icons";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { images } from "@/constants/images";
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  ScrollView,
-  Text,
-  View,
-} from "react-native";
 import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch"; // Custom fetch hook
 import { fetchMovies } from "@/services/api";
@@ -16,60 +9,65 @@ import { fetchMovies } from "@/services/api";
 // Main app screen
 export default function App() {
   const router = useRouter();
-
-  // Fetch popular movies on mount
   const {
     data: movies,
     loading: moviesLoading,
     error: moviesError,
   } = useFetch(() => fetchMovies({ query: "" }));
 
+  if (moviesLoading) {
+    return (
+      <View className="flex-1 bg-slate-950 justify-center items-center">
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>
+    );
+  }
+
+  if (moviesError) {
+    return (
+      <View className="flex-1 bg-slate-950 justify-center items-center">
+        <Text className="text-red-500">Error: {moviesError?.message}</Text>
+      </View>
+    );
+  }
+
   return (
     <View className="flex-1 bg-slate-950">
-      {/* Background image */}
-      <Image source={images.bg} className=" absolute w-full z-0"></Image>
-      <ScrollView
-        className="flex-1 px-5 "
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "flex-start",
+          gap: 20,
+          paddingRight: 5,
+          marginBottom: 10,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 20,
+          paddingHorizontal: 20,
+        }}
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
-      >
-        {/* App logo */}
-        <Image
-          source={icons.logo}
-          className="w-12 h10 mt-20 mb-5 mx-auto"
-        ></Image>
-
-        {/* Loading, error*/}
-        {moviesLoading ? (
-          <ActivityIndicator size="large" color="#00ff00" className="mt-10" />
-        ) : moviesError ? (
-          <Text className="text-red-500 text-center mt-10">
-            {" "}
-            Error: {moviesError?.message}
-          </Text>
-        ) : (
-          <View className="flex-1 mt-5">
+        ListHeaderComponent={
+          <>
+            <Image
+              source={icons.logo}
+              className="w-12 h-10 mt-20 mb-5 mx-auto"
+            />
             <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search for a movie"
-            ></SearchBar>
-            <>
-              <Text className="text-lg text-white font-bold mt-5 mb-3">
-                Latest Movies
-              </Text>
-
-              <FlatList
-                data={movies}
-                renderItem={({ item }) => (
-                  <Text className="text"> {item.title}</Text>
-                )}
-              >
-                {" "}
-              </FlatList>
-            </>
-          </View>
+            />
+            <Text className="text-lg text-white font-bold mt-5 mb-3">
+              Latest Movies
+            </Text>
+          </>
+        }
+        renderItem={({ item }) => (
+          <Text className="text-white text-sm">{item.title}</Text>
         )}
-      </ScrollView>
+      />
     </View>
   );
 }
