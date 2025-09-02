af501614cd04e278cf9fe76ec19580c5a8f2bb26
Jose <Jose0156.114@gmail.com>
2025-09-01 21:35:48 -0600

    WIP search tab

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Image, FlatList } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-slate-950 ">
+      <Image
+        source={images.bg}
+        className=" flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      ></Image>
+      <FlatList data={movies} renderItem={({item}) => <MovieCard {... item}></MovieCard>}>
+
+      </FlatList>
     </View>
   );
 };
